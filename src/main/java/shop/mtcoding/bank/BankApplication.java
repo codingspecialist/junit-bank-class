0099a9b55d7c45d2ec39083c56018e2e8fd7a51f
metacoding <getinthere@naver.com>
2023-01-11 19:05:36 +0900

    프로젝트 생성 및 yml파일 세팅 완료

---
+++ src/main/java/shop/mtcoding/bank/BankApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
